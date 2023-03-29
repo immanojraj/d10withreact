36955515f8dcb2325b6607ffaa87821ca1836bde
immanojraj <77238730+immanojraj@users.noreply.github.com>
2023-03-29 17:39:37 +0530

    Update LayoutsList.js

--- my-app/src/components/Layouts/LayoutsList.js
+++ my-app/src/components/Layouts/LayoutsList.js
@@ -19,7 +19,9 @@ class LayoutsList extends React.Component {
                 <h1>This comes from Drupal API ! Yaay :) </h1>
                 {data !== null &&
                 data !== undefined &&
-                data.length > 0 ? data.map(item => <Layoutitem {...item} key={item.id}/>) : <div>No Drupal Content found.</div>
+                data.length > 0 ? data.map(item => <Layoutitem {...item} key={item.id}/>) 
+                : 
+                <div>No Drupal Content found.</div>
                 }
             </div>
         )
